c375cb6843a0b11a907ac8722c947d45325fcf48
Elijah Tang <elijahtang@gmail.com>
2022-02-24 17:29:26 -0800

    added key attribute to list of elements

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
 
 const monthNames = [
@@ -42,6 +42,7 @@ const Calendar = () => {
         {["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"].map((dayOfWeek) => {
           return (
             <span
+              key={dayOfWeek}
               style={{
                 width: "13.5%",
                 border: "1px solid black",
@@ -141,18 +142,16 @@ const Calendar = () => {
 
     return (
       <span
+        key={dayOfWeek}
         style={{
           width: "13.5%",
           height: "100%",
           border: "1px solid black",
           display: "inline-block",
           cursor: isDisabled ? "unset" : "pointer",
-          backgroundColor: isSelected(
-            cDateObj
-          )
-            ? "lightblue"
-            : backgroundColor
+          backgroundColor: isSelected(cDateObj) ? "lightblue" : backgroundColor
         }}
+        // key={weekNumber*dayOfWeek+currentDate}
         onClick={() => {
           if (isDisabled) {
             // reset start and end date
@@ -185,7 +184,8 @@ const Calendar = () => {
   // render 7 currentDate's in a row of week
   const getWeek = (weekNumber: number) => {
     return (
-      <div style={{ width: "100%", height: "55px" }}>
+      <div key={weekNumber}
+      style={{ width: "100%", height: "55px" }}>
         {[0, 1, 2, 3, 4, 5, 6].map((dayOfWeek) => {
           return getDay(dayOfWeek, weekNumber);
         })}
@@ -206,7 +206,9 @@ const Calendar = () => {
       >
         <button
           onClick={() => {
-            setCurrentDateObj(new Date(currentYear, currentMonth - 1, currentDay));
+            setCurrentDateObj(
+              new Date(currentYear, currentMonth - 1, currentDay)
+            );
           }}
         >
           Prev
@@ -216,22 +218,39 @@ const Calendar = () => {
         </h3>
         <button
           onClick={() => {
-            setCurrentDateObj(new Date(currentYear, currentMonth + 1, currentDay));
+            setCurrentDateObj(
+              new Date(currentYear, currentMonth + 1, currentDay)
+            );
           }}
         >
           Next
         </button>
       </div>
 
-      <div className="selectedRange" style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        gap: "20px"
-      }}>
-        {!startDate && <div><b>Please select start date and end date from the calendar</b></div>}
-        {startDate && <div><b>Start Date:</b> {startDate.toDateString()}</div>}
-        {endDate && <div><b>End Date:</b> {endDate.toDateString()}</div>}
+      <div
+        className="selectedRange"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: "20px"
+        }}
+      >
+        {!startDate && (
+          <div>
+            <b>Please select start date and end date from the calendar</b>
+          </div>
+        )}
+        {startDate && (
+          <div>
+            <b>Start Date:</b> {startDate.toDateString()}
+          </div>
+        )}
+        {endDate && (
+          <div>
+            <b>End Date:</b> {endDate.toDateString()}
+          </div>
+        )}
       </div>
 
       {getHeader()}
